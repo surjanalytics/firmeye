276a0a4dbb162b2919cdbc3f57248294407b5d8b
Adam Janicki <adamjanicki2@gmail.com>
2023-11-16 11:41:45 -0500

    copy over firmeye

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Alert from "src/components/modules/Alert";
+import Footer from "src/components/modules/Footer";
+import Home from "src/components/pages/Home";
+import Nav from "src/components/modules/Nav";
+import NotFound from "src/components/pages/NotFound";
+import About from "src/components/pages/About";
+import Upload from "src/components/pages/Upload";
+
+const App = () => (
+  // Configure basename to use GitHub Pages
+  <BrowserRouter basename="/firmeye">
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Alert />
+    <Footer />
+  </BrowserRouter>
+);
+
+export default App;
